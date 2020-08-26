fc00fc7fb28079590a061a3aabefc071d6e011e4
Megan Horsfield <meganthadev@gmail.com>
2020-08-26 06:55:31 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
+import MovieShow from '../components/MovieShow';
 
 const MoviesPage = ({ match, movies }) => (
   <div>
